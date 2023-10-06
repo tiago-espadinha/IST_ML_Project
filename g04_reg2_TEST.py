0d5bf3efec1ea72d2a62e7998ccc3c074db7570c
tmvf21 <121057196+tmvf21@users.noreply.github.com>
2023-10-06 21:29:11 +0100

    feat: add Reg2 regression with tests

--- g04_reg2_TEST.py
+++ g04_reg2_TEST.py
@@ -1,10 +1,3 @@
-'''
-Machine Learning - Linear Regression Problem 2
-Authors:
-Tiago Simões, 96329
-Tomás Fonseca, 66325
-'''
-
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
@@ -13,7 +6,7 @@ from sklearn.linear_model import LinearRegression, RANSACRegressor, LassoCV
 from sklearn.model_selection import cross_val_score
 import seaborn as sns
 
-path = 'lab2/' # Change path to the folder where the data is located
+path = 'lab2/'
 scale = True
 
 def error_metrics(y_real, y_pred_c1, y_pred_c2, y_pred, inlier_mask):
@@ -37,7 +30,7 @@ def cv_metrics(model, x, y, k):
     folds_avg_mse = abs(cv_results)
     max_avg_mse = np.max(folds_avg_mse)
     return avg_mse, folds_avg_mse, max_avg_mse
-
+    
 def ransac_classifier(x_train, y_train, res_threshold):
     ransac = RANSACRegressor(random_state = 1, residual_threshold = res_threshold).fit(x_train, y_train)
     inlier_mask = ransac.inlier_mask_
@@ -71,11 +64,12 @@ def main():
     X_train = np.load(path + 'X_train_regression2.npy')
     y_train = np.load(path + 'y_train_regression2.npy')
     X_test = np.load(path + 'X_test_regression2.npy')
-
+    y_test = np.load(path + 'y_test_regression2.npy')
+    
     X_scaler = StandardScaler()
     y_scaler = StandardScaler()
 
-    if scale:
+    if scale == True:
         X_train_scaled = X_scaler.fit(X_train).transform(X_train)
         y_train_scaled = y_scaler.fit(y_train).transform(y_train)
         X_test_scaled = X_scaler.transform(X_test)
@@ -84,84 +78,34 @@ def main():
         y_train_scaled = y_train
         X_test_scaled = X_test
 
-    print('-------------------------------------------------------')
-    print('RANSAC Regression')
+    alpha_c1 = 0.6
+    alpha_c2 = 0.7
     
-    alphas = np.arange(0.4, 1.2, 0.1)
-    best_MSE = 1e100
-
-    for alpha_c1 in alphas:
-        
-        ransac1, x_cluster1_scaled1, x_cluster2_scaled1, y_cluster1_scaled1, y_cluster2_scaled1, inlier_mask = ransac_classifier(X_train_scaled, y_train_scaled.ravel(), alpha_c1)
-
-        regr1 = ransac1.fit(x_cluster1_scaled1, y_cluster1_scaled1)
-
-        MSE1, _, max_MSE1 = cv_metrics(regr1, x_cluster1_scaled1, y_cluster1_scaled1.ravel(), x_cluster1_scaled1.shape[0])
-        
-        for alpha_c2 in alphas:
-            ransac2, x_cluster1_scaled2, _, y_cluster1_scaled2, _, _ = ransac_classifier(x_cluster2_scaled1, y_cluster2_scaled1.ravel(), alpha_c2)
-
-            regr2 = ransac2.fit(x_cluster1_scaled2, y_cluster1_scaled2)
-
-            MSE2, _, max_MSE2 = cv_metrics(regr2, x_cluster2_scaled1, y_cluster2_scaled1.ravel(), x_cluster2_scaled1.shape[0])
-
-            if MSE1 + MSE2 < best_MSE:
-                best_MSE = MSE1 + MSE2
-                best_alpha_c1 = alpha_c1
-                best_alpha_c2 = alpha_c2
-
-    ransac1, x_cluster1_scaled1, x_cluster2_scaled1, y_cluster1_scaled1, y_cluster2_scaled1, inlier_mask = ransac_classifier(X_train_scaled, y_train_scaled.ravel(), best_alpha_c1)
+    ransac1, x_cluster1_scaled1, x_cluster2_scaled1, y_cluster1_scaled1, y_cluster2_scaled1, inlier_mask = ransac_classifier(X_train_scaled, y_train_scaled.ravel(), alpha_c1)
     regr1 = ransac1.fit(x_cluster1_scaled1, y_cluster1_scaled1)
-    MSE1, _, max_MSE1 = cv_metrics(regr1, x_cluster1_scaled1, y_cluster1_scaled1.ravel(), x_cluster1_scaled1.shape[0])
-
-    ransac2, x_cluster1_scaled2, _, y_cluster1_scaled2, _, _ = ransac_classifier(x_cluster2_scaled1, y_cluster2_scaled1.ravel(), best_alpha_c2)
-    regr2 = ransac2.fit(x_cluster1_scaled2, y_cluster1_scaled2)
-    MSE2, _, max_MSE2 = cv_metrics(regr2, x_cluster2_scaled1, y_cluster2_scaled1.ravel(), x_cluster2_scaled1.shape[0])
     
+    ransac2, x_cluster1_scaled2, _, y_cluster1_scaled2, _, _ = ransac_classifier(x_cluster2_scaled1, y_cluster2_scaled1.ravel(), alpha_c2)
+    regr2 = ransac2.fit(x_cluster1_scaled2, y_cluster1_scaled2)
+
     print('x_cluster1.shape\t= ', x_cluster1_scaled1.shape)
     print('y_cluster1.shape\t= ', y_cluster1_scaled1.shape)
     print('x_cluster2.shape\t= ', x_cluster1_scaled2.shape)
     print('y_cluster2.shape\t= ', y_cluster1_scaled2.shape)
 
-    regr3 = LinearRegression().fit(X_train_scaled, y_train_scaled.ravel())
-    MSE3, _, max_MSE3 = cv_metrics(regr3, X_train_scaled, y_train_scaled.ravel(), 10)
-
-    print('best_alpha_c1\t= ', best_alpha_c1)
-    print('best_alpha_c2\t= ', best_alpha_c2)
-    
-    y_pred1_scaled = regr1.predict(X_train_scaled).reshape(y_train.shape[0], 1)
-    y_pred2_scaled = regr2.predict(X_train_scaled).reshape(y_train.shape[0], 1)
-    y_pred3_scaled = regr3.predict(X_train_scaled).reshape(y_train.shape[0], 1)
-    
-    if scale:
-        y_pred1 = y_scaler.inverse_transform(y_pred1_scaled)
-        y_pred2 = y_scaler.inverse_transform(y_pred2_scaled)
-        y_pred3 = y_scaler.inverse_transform(y_pred3_scaled)
-    else:
-        y_pred1 = y_pred1_scaled
-        y_pred2 = y_pred2_scaled
-        y_pred3 = y_pred3_scaled
-
-    print('cv_MSE\t= ', MSE1, '\t\t;\t', MSE2, '\t;\t', MSE3)
-    print('max_MSE\t= ', max_MSE1, '\t\t;\t', max_MSE2, '\t;\t', max_MSE3)
-    
-    error_metrics(y_train.ravel(), y_pred1.ravel(), y_pred2.ravel(), y_pred3.ravel(), inlier_mask)
-
     y_test_pred1_scaled = regr1.predict(X_test_scaled).reshape(X_test_scaled.shape[0], 1)
     y_test_pred2_scaled = regr2.predict(X_test_scaled).reshape(X_test_scaled.shape[0], 1)
 
-    if scale:
+    if scale == True:
         y_test_pred1 = y_scaler.inverse_transform(y_test_pred1_scaled)
         y_test_pred2 = y_scaler.inverse_transform(y_test_pred2_scaled)
     else:
         y_test_pred1 = y_test_pred1_scaled
         y_test_pred2 = y_test_pred2_scaled
-
+    
     y_test_pred = np.hstack((y_test_pred1, y_test_pred2))
-    np.save(path + 'y_test_regression2.npy', y_test_pred)
-
-    # plt.show()
-    print('-------------------------------------------------------')
+    
+    print(np.sum(np.subtract(y_test[0], y_test_pred[0])))
+    print(np.sum(np.subtract(y_test[1], y_test_pred[1])))
 
 if __name__ == "__main__":
     main()
